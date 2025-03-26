20956442e9465a23acee75a7a2aabb4a75cccdcf
sabsiru <zero013@naver.com>
2025-03-26 13:09:49 +0900

    #1 포인트 충전/사용 서비스
    포인트 충전/사용 서비스 코드 작성 및 테스트 코드 작성

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,9 +8,38 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class PointService {
-    final UserPointTable userPointTable;
 
+    final UserPointTable userPointTable;
+    final PointHistoryTable pointHistoryTable;
+
+    //포인트 조회
     public UserPoint getUserPoint(long userId) {
         return userPointTable.selectById(userId);
+    }
+
+    //포인트 충전
+    public UserPoint chargePoint(UserPoint current, long amount) {
+
+        UserPoint userPoint = userPointTable.selectById(current.id());
+        UserPoint charged = UserPoint.charge(userPoint, amount);
+
+        userPointTable.insertOrUpdate(charged.id(),charged.point());
+
+        pointHistoryTable.insert(charged.id(),amount,TransactionType.CHARGE, System.currentTimeMillis());
+
+        return charged;
+    }
+
+    //포인트 사용
+    public UserPoint usePoint(UserPoint current, long amount) {
+
+        UserPoint userPoint = userPointTable.selectById(current.id());
+        UserPoint used = UserPoint.use(userPoint, amount);
+
+        userPointTable.insertOrUpdate(used.id(),used.point());
+
+        pointHistoryTable.insert(used.id(),amount,TransactionType.USE, System.currentTimeMillis());
+
+        return used;
     }
 }
--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,5 +1,6 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,13 +9,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class PointServiceTest {
 
     @Mock
     private UserPointTable userPointTable;
+
+    @Mock
+    private PointHistoryTable pointHistoryTable;
 
     @InjectMocks
     private PointService pointService;
@@ -45,6 +50,101 @@ class PointServiceTest {
         //then
         assertEquals(0,result.point());
         assertEquals(99L,result.id());
-        
+
     }
+
+    @Test
+    public void 포인트_충전_성공() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 1000L, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        UserPoint result = pointService.chargePoint(current, 3000L);
+
+        //then
+        assertEquals(4000L,result.point());
+        assertEquals(1L,result.id());
+
+    }
+
+    @Test
+    public void 충전시_최대한도_초과시_예외발생() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 90000L, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> pointService.chargePoint(current, 10001L));
+
+        //then
+        assertEquals("충전 가능한 최대 포인트는 100,000입니다.", e.getMessage());
+    }
+    
+    @Test
+    public void 충전_성공시_히스토리_저장_성공() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 1000L, System.currentTimeMillis());
+
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        UserPoint charged = pointService.chargePoint(current, 3000L);
+
+        //then
+        verify(pointHistoryTable, times(1)).insert(
+                eq(1L),
+                eq(3000L),
+                eq(TransactionType.CHARGE),
+                anyLong()
+        );
+    }
+
+    @Test
+    public void 포인트_사용_성공() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 5000L, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        UserPoint used = pointService.usePoint(current, 3000L);
+
+        //then
+        assertEquals(2000L,used.point());
+        assertEquals(1L,used.id());
+    }
+    
+    @Test
+    public void 포인트_사용시_잔고가_0미만일시_예외발생() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 1000L, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> pointService.usePoint(current, 1001L));
+
+        //then
+        assertEquals("포인트가 부족합니다.", e.getMessage());
+    }
+
+    @Test
+    public void 포인트_사용성공시_히스토리_저장_성공() throws Exception{
+        //given
+        UserPoint current = new UserPoint(1L, 4000L, System.currentTimeMillis());
+        when(userPointTable.selectById(1L)).thenReturn(current);
+
+        //when
+        UserPoint used = pointService.usePoint(current, 4000L);
+
+        //then
+        verify(pointHistoryTable, times(1)).insert(
+                eq(1L),
+                eq(4000L),
+                eq(TransactionType.USE),
+                anyLong()
+        );
+    }
+
 }
